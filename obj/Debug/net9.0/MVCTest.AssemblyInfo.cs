d322bb0d3cbbc2a41f644d062ee01af99f43da28
Travis Allan <travisleeallan@gmail.com>
2025-06-12 23:21:44 -0500

    added emoji tetris

--- obj/Debug/net9.0/MVCTest.AssemblyInfo.cs
+++ obj/Debug/net9.0/MVCTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cf9b9df81d83a3321734f28cc814a065f59c54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
